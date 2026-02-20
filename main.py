4c8787f6f982a254ac88ff1c4bb1c4e54e3a2efd
-THE-CODER- <14734752--THE-CODER-@users.noreply.replit.com>
2026-02-20 17:11:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,131 @@
+import os
+import sys
+import pygame # main lib for rendering
+from world import *
+import utilies
+
+class Geometry_dash:
+
+    def __init__(self):
+
+        # screen init
+        pygame.init()
+        self.screen = pygame.display.set_mode((1200,800), pygame.RESIZABLE)
+        pygame.display.set_caption('Geometry Dash')
+
+        #timing
+        self.clock = pygame.time.Clock()
+        self.max_fps = 60 #tick rate
+        self.dt = 0
+
+        #making of the player
+        self.player = pygame.Rect(100, 0, 50, 50) # x, y, w, h ---> this is the hitbox
+        self.player_imgs = utilies.SpriteSheet()
+        self.player_imgs.extract_single_image("player_sprites/Simple_cube.jpg", self.player.size, 255)
+        self.rotation = 0
+        self.GRAVITY = -9.8 # all caps = constant
+        self.velocity = pygame.Vector2(0, 0)
+        self.mass = 3
+        self.jump = False
+        self.mode = "Cube"
+
+        #making of ground
+        self.ground = pygame.Rect(0, 480, 1200, 800)
+
+
+        #Editor
+        self.world = Editor(1, False, self.ground.y) #KIAN IT DOESNT WORK, only W and A keys work and super glichy and slow, I am disabling it for now
+        self.debug = True
+
+    
+    def run(self):
+        while True:
+            self.screen.fill((116,75,196)) #rgb - 255-0
+            self.dt = self.clock.tick(60)/1000 # the number inside is the fps or will also tick rate
+ 
+            #update physics or level editor
+            if not self.world.editor:
+                self.apply_player_physics()
+            else:
+                self.world.level_editor()
+            self.world.update_world(5)
+            #event handling
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+                    if event.key == pygame.K_SPACE and self.jump:
+                        self.velocity.y = -15
+                        self.jump = False
+
+
+                if event.type == pygame.MOUSEBUTTONDOWN and self.jump:
+
+                        self.velocity.y = -15
+                        self.jump = False
+            
+            #draw stuff
+            self.draw()
+
+            pygame.display.update()
+            if self.debug:
+                print(self)
+                print(self.world)
+                print(f"Vel: {self.velocity} \nDt: {self.dt} \nCan Jump: {self.jump} \nOS: {os.name}")
+                os.system('cls' if os.name == 'nt' else 'clear')
+
+    def draw(self):
+        if self.world.editor:
+            self.world.draw_grid()
+
+        #draw player
+        #add rotation to player
+        if not self.jump:
+            self.rotation = (self.rotation - 600 * self.dt) % 360
+        self.player_imgs.images[0] = pygame.transform.rotate(
+            self.player_imgs.original_image[0],
+            self.rotation
+        ).convert_alpha()
+        
+        self.screen.blit(self.player_imgs.get_image(0).convert_alpha(), self.player) #type: ignore
+        if self.debug: pygame.draw.rect(self.screen, (255,0,0), self.player, 2)
+            
+        pygame.draw.rect(self.screen, (0,225,0), self.ground)
+        
+       
+    def apply_player_physics(self):
+
+        #update player first
+        self.player.move_ip(self.velocity)
+
+        #gravity
+        self.velocity.y -= self.GRAVITY * self.mass * self.dt
+        self.velocity.y = min(self.velocity.y, 50)
+
+        #ground colliton
+        if self.player.colliderect(self.ground) : # rect vs rect: if collide => True no collide => False
+            self.jump = True
+            self.rotation = 0
+            self.velocity.y = 0
+            self.player.y = (self.ground.y - self.player.h)
+
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    app = Geometry_dash()
+    app.run()
+
+
+
+
---
+++ world.py
@@ -0,0 +1,157 @@
+import pygame
+import math
+import Base_worlds
+
+
+class Editor:
+
+    def __init__(self, level, editor, ground_y):
+        self.level = level  #Int
+        self.screen = pygame.display.get_surface()
+        
+        #Editor
+        self.editor = editor  #Bool
+        self.editor_window = True
+        self.offsetx = 0
+        self.offsety = 0
+
+        #Editor Values
+        self.block = "01"  #This is temporary until further progress is made in the editor
+        self.color = (0, 0, 0)
+        self.color_alpha = 255
+        self.outline = (255, 255, 255)
+        self.outline_alpha = 255
+        self.speed = 5
+        self.grid = 40
+        self.gridx = self.grid - self.screen.get_width() % self.grid
+        self.gridy = self.grid - self.screen.get_height() % self.grid
+        self.size_multiplier = 1
+
+        #world attr
+        self.level = Base_worlds.test
+        self.ground_y = ground_y-self.grid
+        self.x = self.screen.get_size()[0]
+
+        #world objects
+        self.spike = []
+        self.blocks = []
+        self.get_world()
+
+    
+    def level_editor(self):
+        if self.editor:
+            mouse = pygame.mouse.get_pos()
+            keys = pygame.key.get_pressed()
+
+            #Basic Editor Shortcuts
+            if keys[pygame.K_p]:
+                self.editor_window = not self.editor_window  #Opens/Closes editor window
+            if keys[pygame.K_PLUS]:
+                if self.grid < 100: self.grid += 20
+            elif keys[pygame.K_MINUS]:
+                if self.grid > 20: self.grid -= 20
+            if keys[pygame.K_UP]:
+                if self.size_multiplier < 3: self.size_multiplier += 0.1
+            elif keys[pygame.K_DOWN]:
+                if self.size_multiplier > 0.1: self.size_multiplier -= 0.1
+            if keys[pygame.K_LEFT]:
+                if len(self.block) > 2: self.block = self.block[1:]
+            elif keys[pygame.K_RIGHT]:
+                if len(self.block) < 3: self.block = "0" + self.block
+            if keys[pygame.K_e]:
+                return ("Add", mouse)
+            elif keys[pygame.K_q]:
+                return ("Delete", mouse)
+
+            #Editor Movement
+            if keys[pygame.K_w]:
+                if self.offsety < 300: self.offsety -= 5
+            if keys[pygame.K_s]:
+                if self.offsety > 0: self.offsety += 5
+            if keys[pygame.K_a]:
+                if self.offsetx < 300: self.offsetx -= 5
+            if keys[pygame.K_d]:
+                if self.offsetx > 0: self.offsetx += 5
+
+    def draw_grid(self):
+        screen = self.screen
+        linewidth = int(math.ceil(self.grid / 20))
+
+        for x in range(int(screen.get_width() / self.gridx)):
+            xl = x * self.gridx + (self.offsetx % self.grid)
+            yl = screen.get_height()
+
+            if xl + self.offsetx == screen.get_width() / 2:
+                color = (0, 255, 0)
+                font = pygame.font.SysFont("Arial", 20)
+                text = font.render("Y", True, color)
+                screen.blit(text, (xl, 20))
+                screen.blit(text, (xl, yl - 20))
+            else:
+                color = (0, 0, 255)
+
+            pygame.draw.line(screen, color, (xl, 0), (xl, yl), linewidth)
+
+        for y in range(int(screen.get_height() / self.gridy)):
+            yl = y * self.gridy + (self.offsety % self.grid)
+            xl = screen.get_width()
+
+            if yl + self.offsety == screen.get_height() / 2:
+                color = (255, 0, 0)
+                font = pygame.font.SysFont("Arial", 20)
+                text = font.render("X", True, color)
+                screen.blit(text, (20, yl))
+                screen.blit(text, (xl - 20, yl))
+            else:
+                color = (0, 0, 255)
+
+            pygame.draw.line(screen, color, (0, yl), (xl, yl), linewidth)
+
+    def get_world(self):
+        for y, row in enumerate(self.level):
+            for x, block in enumerate(row):
+                match block:
+                    case 1:
+                        # (5 - y) because the list has 6 rows (0 to 5)
+                        # Row 5 in the list should be just above ground
+                        self.spike.append(Spike((x * self.grid), self.ground_y - ((5 - y) * self.grid), self.grid))
+                    case 2:
+                        self.blocks.append(pygame.Rect((x * self.grid), self.ground_y - ((5 - y) * self.grid), self.grid, self.grid))
+                    case _:
+                        continue
+    def update_world(self, speed):
+        # self.x -= speed # Removed this as it was causing confusion with individual object positions
+        for spike in self.spike:
+            spike.move(-speed, 0)
+            spike.draw(self.screen)
+        for block in self.blocks:
+            block.move_ip(-speed, 0)
+            pygame.draw.rect(self.screen, (255, 255, 255), block)
+    def __str__(self):
+        return f"Block: {self.block}"
+
+class Spike:
+
+    def __init__(self, x, y, size):
+        self.x = x
+        self.y = y
+        self.size = size
+
+        self.vert = [
+            [self.x + self.size // 2, self.y],
+            [self.x, self.y + self.size],
+            [self.x + self.size, self.y + self.size],
+        ]
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+
+        self.vert = [
+            [self.x + self.size // 2, self.y],
+            [self.x, self.y + self.size],
+            [self.x + self.size, self.y + self.size],
+        ]
+        
+    def draw(self, screen):
+        pygame.draw.polygon(screen, (255, 255, 255), self.vert)
